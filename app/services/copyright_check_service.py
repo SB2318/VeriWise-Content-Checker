fc2d5db56bade464acda4a50f9bf27a7c1227cec
SB2318 <sb2318@it.jgec.ac.in>
2025-05-23 09:26:59 +0530

    --image--copyright--checker--work--start

---
+++ app/services/copyright_check_service.py
@@ -0,0 +1,6 @@
+
+# The service will only help you to detect any watermark, logo, text with copyright symbol present
+# in the image. It will not remove the watermark or logo from the image.
+
+class CopyrightCheckerService:
+    
