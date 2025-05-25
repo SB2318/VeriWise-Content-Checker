6d6e638e40e181194e1770d990aae3686e4e1c76
SB2318 <sb2318@it.jgec.ac.in>
2025-05-26 01:28:29 +0530

    --cors--added

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,19 @@ WORKDIR /app
 
 COPY requirements.txt .
 
+
+
 # Install libGL and other dependencies needed for OpenCV
 RUN apt-get update && \
+    apt-get install -y default-jre libgl1 libglib2.0-0 && \
     apt-get install -y libgl1 libglib2.0-0 && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
-RUN pip install --no-cache-dir -r requirements.txt
+
+RUN pip install --no-cache-dir -r requirements.txt && \
+    rm -rf /root/.cache/pip
+
 
 COPY . .
 
--- app/__init__.py
+++ app/__init__.py
@@ -10,12 +10,22 @@
 
 from fastapi import FastAPI
 from app.routes.routes import register_routes
-
+from fastapi.middleware.cors import CORSMiddleware
 def create_app():
     app = FastAPI(
         title="Content Intelligence API",
         description="API for Content verification",
         version="1.0.0",
+    )
+    origins = [
+    "https://veriwise-swagger-ui.vercel.app",  
+    ]
+    app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
     )
     register_routes(app)
     return app
